9faa7fc357ffd3f35c3048d940e13fc644210d34
md ismail <mdismail85773@gmail.com>
2025-10-14 00:43:47 +0800

    single post get and show page added

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const page = ({params}) => {
-    const  {id}=  params;
-    console.log(id);
-    return (
-        <div>
-            
-        </div>
-    );
+const getSinglePost = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  return res.json();
 };
 
-export default page;
+const Page = async ({ params }) => {
+  const { id } = params;
+
+  const post = await getSinglePost(id);
+    // console.log(post);
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-2 text-center">Post #{id}</h1>
+      <p className="text-center">{post.body}</p>
+    </div>
+  );
+};
+
+export default Page;
