cc21c81e20fa2a66a3fd2891b8ef6bbbf01d2311
md ismail <mdismail85773@gmail.com>
2025-10-13 19:58:29 +0800

    get posts from database using api

---
+++ src/app/posts/[id]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = ({params}) => {
+    const  {id}=  params;
+    console.log(id);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default page;
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,27 @@
+
+import Link from 'next/link';
 import React from 'react';
 
-const page = () => {
-    return (
-        <div>
-            this is posts page
-        </div>
-    );
+const getPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return res.json();
 };
 
-export default page;
+const Page = async () => {
+  const posts = await getPosts();
+//   console.log(posts);
+
+  return (
+    <div className='grid grid-cols-4 gap-5'>
+      {posts.map(post => (
+        <div key={post.id} className='p-5 border border-gray-300 rounded-md'>
+        <h1 className='text-2xl text-amber-100'>{post.title}</h1>
+        <p>{post.body}</p>
+        <Link href={`/posts/${post.id}`} className='text-blue-500 underline'>Read More</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Page;
